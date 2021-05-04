8e91302322ce5cf4f644382fdcd1d5ea1cd5771f
riri3 <kamijooor@gmail.com>
2021-05-04 16:09:21 +0000

    [update]3-8flashメッセージ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,12 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -24,8 +28,12 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      render :new
+    end
   end
   
   def destroy
