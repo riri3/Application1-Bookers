fcb73d736acf38e06df2404d1df9ea1840804a21
riri3 <kamijooor@gmail.com>
2021-05-05 08:14:30 +0000

    [update]3-8 indexに新規投稿画面挿入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def new
@@ -13,10 +14,10 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:success] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :new
     end
@@ -27,10 +28,10 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:success] = "Book was successfully updated."
-      redirect_to book_path(book)
+      redirect_to book_path(@book.id)
     else
       render :new
     end
